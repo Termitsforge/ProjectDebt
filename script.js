4a7fc57c42f5fcd847cf83fc131bade8a2996a72
Termitsforge <61342523+Termitsforge@users.noreply.github.com>
2021-02-18 23:42:34 +0300

    sing_in_done!

---
+++ script.js
@@ -0,0 +1,9 @@
+let show_password = function(){
+    var input = document.getElementById("pass");
+    var checkbox = document.getElementById("check");
+    if(checkbox.checked) input.setAttribute('type', 'text');
+    else input.setAttribute('type','password');
+}
+
+
+
