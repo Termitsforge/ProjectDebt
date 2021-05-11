49d9c7e6dfd705a6170d112e8a6afadfb4861dcd
Егор Орехов <termitsforge@yandex.ru>
2021-05-11 14:33:11 +0300

    Подключил базу данных

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import hbs from 'hbs';
 import mysql from 'mysql2';
+import bcrypt from 'bcrypt';
 
 let user = {},
     count_users = 0;
-
 const app = express();
 const PORT = 3000;
 const _dirname = path.resolve();
@@ -16,7 +16,7 @@ const connection = mysql.createConnection({
     database: "debts_project",
     password: "root"
 });
-
+const saltRounds = 10;
 const Parser = bodyParser.urlencoded({
     extended: false
 });
@@ -25,11 +25,31 @@ hbs.registerPartials(_dirname + "/views/partials");
 app.use(express.static(path.join(_dirname, '/public')));
 /*Sing In */
 app.get('/', (req, res) => {
-    res.render('sing_in.hbs');
+    res.redirect('/sing_in');
+});
+app.get('/sing_in', (req, res) => {
+    res.render('sing_in.hbs',{
+        isVisible: false
+    });
 });
 app.post('/sing_in', Parser, (req, res) => {
-    user.name = req.body.log_in;
-    res.redirect('/main');
+    let resultsQuery;
+    connection.query("SELECT log_in, password FROM users WHERE log_in = ?;", [req.body.log_in], function (err, results, fields) {
+        resultsQuery = results[0];
+        if (resultsQuery) {
+            bcrypt.compare(req.body.pass, resultsQuery.password, (err, results) => {
+                if (results) {
+                    user.name = req.body.log_in;
+                    res.redirect('/main');
+                } else {
+                    res.render('sing_in.hbs', {
+                        isVisible: true
+                    });
+                }
+            });
+        }
+    
+    });
 });
 /*Sing_up*/
 app.get('/sing_up', (req, res) => {
@@ -39,14 +59,18 @@ app.post('/sing_up', Parser, (req, res) => {
     user.name = req.body.log_in;
     connection.query("SELECT COUNT(*) as count FROM users", function (err, results, fields) {
         count_users = results[0].count;
-         // число пользователей
+        // число пользователей
+        connection.query("INSERT INTO users VALUES(?,?,?,?)", [count_users += 1, req.body.log_in, bcrypt.hashSync(req.body.pass, saltRounds), req.body.email], function (err, results) {
+            if (err) console.error(err);
+            else{
+                res.redirect('/main');
+                console.log("Данные добавлены");
+            } 
+        });
+        
     });
-    console.log(count_users);
-    connection.query("INSERT INTO users VALUES(?,?,?,?)", [count_users+=1, req.body.log_in, req.body.pass, req.body.email], function (err, results) {
-        if (err) console.log(err);
-        else console.log("Данные добавлены");
-    });
-    res.redirect('/main');
+
+    
 });
 /*Main*/
 app.get('/main', Parser, (req, res) => {
@@ -54,7 +78,6 @@ app.get('/main', Parser, (req, res) => {
         name: user.name
     });
 });
-
 /*Notebook*/
 app.get('/notebook', (req, res) => {
     res.render('notebook.hbs', {
